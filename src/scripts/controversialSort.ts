a61a1b57da4a99690361eec568b8aced33ad4930
Andrew Dutson <Dutsandrew@gmail.com>
2023-01-14 14:57:06 -0700

    controversial sorts by ratio of dislikes : likes instead of just most dislikes

--- src/scripts/controversialSort.ts
+++ src/scripts/controversialSort.ts
@@ -4,11 +4,11 @@ export default function controversialSort(postsArray: any[]): any[] {
 
   const sortedArray = postsArray.sort((post1: Post, post2: Post): number => {
 
-    if (post1.dislikes > post2.dislikes) {
+    if ((post1.dislikes - post1.likes) > (post2.dislikes - post2.likes)) {
       return -1;
     };
 
-    if (post2.dislikes > post1.dislikes) {
+    if ((post2.dislikes - post2.likes) > (post1.dislikes - post1.likes)) {
       return 1;
     }
 
